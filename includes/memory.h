9f00afd306d57554bdca77653a391f91d2ab6bf9
Fureimu <yann.guinio@gmail.com>
2025-01-29 13:36:31 +0100

    Reorganised get_next_line

--- includes/memory.h
+++ includes/memory.h
@@ -6,7 +6,7 @@
 /*   By: fureimu <fureimu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 10:29:30 by fureimu           #+#    #+#             */
-/*   Updated: 2025/01/29 13:25:29 by fureimu          ###   ########.fr       */
+/*   Updated: 2025/01/29 13:36:11 by fureimu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+char	*get_next_line(int fd);
 
 #endif
