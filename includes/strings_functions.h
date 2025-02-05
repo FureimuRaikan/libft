f11c80d660c508f6c8cd7cf49b7884b2e5b88d72
Fureimu <yguinio@student.42.fr>
2025-02-05 15:52:50 +0100

    Saved the 2025-02-05 at 15:52:50 from paul-f5Br6s5.clusters.42paris.fr

--- includes/strings_functions.h
+++ includes/strings_functions.h
@@ -28,6 +28,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr_free(char *str, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_reverse(char *str);
 
